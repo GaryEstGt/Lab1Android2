182d5644f704cd7cb021ba52eae4829759f1cbb4
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2018-09-01 23:17:22 -0600

    Lista y busqueda version 2

--- app/src/main/java/com/example/garya/lab1android/MainActivity.java
+++ app/src/main/java/com/example/garya/lab1android/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.garya.lab1android;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -19,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.ListSong)
     ListView ListSong;
     Biblioteca canciones = new Biblioteca();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,15 +36,21 @@ public class MainActivity extends AppCompatActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btnBuscar:
-                    String nombre;
-                    nombre=txtBusca.getText().toString();
-                    String dato[]=new String[]{ canciones.getBiblioteca().get(nombre).toString()};
+                String nombre;
+                nombre = txtBusca.getText().toString();
+                String dato[] = new String[]{canciones.getBiblioteca().get(nombre).toString()};
+                Cancion find[]= new Cancion[]{canciones.getBiblioteca().get(nombre)};
 
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,dato);
-                ListSong.setAdapter(adapter );
+                ArrayAdapter<Cancion> adapter = new ArrayAdapter<Cancion>(this, android.R.layout.simple_list_item_1,find);
+                ListSong.setAdapter(adapter);
                 break;
             case R.id.btnPlaylist:
                 break;
         }
     }
+
+    //@OnClick(R.id.ListSong)
+    //public void onViewClicked() {
+
+    //}
 }
